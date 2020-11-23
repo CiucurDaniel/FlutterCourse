690d25a1ae133d1936a434ae2d59692055994477
Ciucur Daniel <ciucur.daniel14@gmail.com>
2020-11-23 22:15:29 +0200

    yaml file

--- lib/Course2/movieJsonExample.dart
+++ lib/Course2/movieJsonExample.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 
-void main() async {
+Future<void> main() async {
   // perform http get
   final http.Response response =
   await http.get('https://yts.mx/api/v2/list_movies.json');
@@ -10,14 +10,38 @@ void main() async {
   //response.body e type string si atunci folosim jsonDecode sa il facem json
   final Map<String, dynamic> responseMap = jsonDecode(response.body);
 
+  // Go deeper at the data property
+  final Map<String, dynamic> data = responseMap['data'];
+
+  // Get the movies inside a list
+  final List<dynamic> movies = data['movies'];
+
+  final List<Movie> movieList = <Movie>[];
+
+  for( int i = 0 ; i < movies.length; i++){
+    final Map<String, dynamic> item = movies[i];
+
+    final Movie movie = Movie(
+      id: item['id'],
+      title: item['title'],
+      year: item['year'],
+      runtime: item['runtime'],
+      cover: item['cover'],
+    ); // Movie
+
+    movieList.add(movie);
+  }
+
   //print(response.body);
   //print(responseMap['status']);
   //print(responseMap['data']['movies'][0]['title']);
+  //String firstMovie = await response['data']['movies'][0]['title'];
 
-  final List<Movie> movieList = <Movie>[];
+  movieList.forEach(print);
 }
 
-//String firstMovie = await response['data']['movies'][0]['title'];
+
+// Movie model
 
 class Movie {
   Movie({
@@ -33,4 +57,9 @@ class Movie {
   final int year;
   final int runtime;
   final String cover;
+
+  @override
+  String toString(){
+    return 'Movie: $title, year: $year, runtime: $runtime, $cover';
+  }
 }

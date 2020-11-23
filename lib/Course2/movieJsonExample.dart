47e3af16118e114eb5cf6bb1fe2522f230d71453
Ciucur Daniel <ciucur.daniel14@gmail.com>
2020-11-23 21:39:25 +0200

    yaml file

--- lib/Course2/movieJsonExample.dart
+++ lib/Course2/movieJsonExample.dart
@@ -1,12 +1,36 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
 
 void main() async {
-  final http.Response response = await http.get('https://yts.mx/api/v2/list_movies.json');
+  // perform http get
+  final http.Response response =
+  await http.get('https://yts.mx/api/v2/list_movies.json');
+
+  //response.body e type string si atunci folosim jsonDecode sa il facem json
   final Map<String, dynamic> responseMap = jsonDecode(response.body);
+
   //print(response.body);
-  print(responseMap['status']);
-  print(responseMap['data']['movies'][0]['title']);
+  //print(responseMap['status']);
+  //print(responseMap['data']['movies'][0]['title']);
+
+  final List<Movie> movieList = <Movie>[];
 }
 
 //String firstMovie = await response['data']['movies'][0]['title'];
+
+class Movie {
+  Movie({
+    @required this.id,
+    @required this.title,
+    @required this.year,
+    @required this.runtime,
+    @required this.cover
+  });
+
+  final int id;
+  final String title;
+  final int year;
+  final int runtime;
+  final String cover;
+}
